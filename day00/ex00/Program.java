336d5cd18ec3d6132ed829d0d78976412970b9ab
teusebio <teusebio@student.21-school.ru>
2022-05-01 14:15:16 +0300

    remove package

--- day00/ex00/Program.java
+++ day00/ex00/Program.java
@@ -1,20 +1,26 @@
-package ex00;
 
 public class Program {
     public static void main(String[] args) {
+
         int number = 479598;
         int sum = 0;
+
         sum += number % 10;
         number /= 10;
         sum += number % 10;
+
         number /= 10;
         sum += number % 10;
+
         number /= 10;
         sum += number % 10;
+
         number /= 10;
         sum += number % 10;
+
         number /= 10;
         sum += number % 10;
+
         System.out.println(sum);
     }
 }
