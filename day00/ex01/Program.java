50030d084dd076e65bb0e0140f2696f6c4a27615
teusebio <teusebio@student.21-school.ru>
2022-05-01 14:38:51 +0300

    remove package

--- day00/ex01/Program.java
+++ day00/ex01/Program.java
@@ -1,31 +1,44 @@
-package ex01;
 
 import java.util.Scanner;
 
 public class Program {
+
+    public static final String ILLEGAL_ARGUMENT = "Illegal Argument";
+    public static final String TRUE = "true ";
+    public static final String FALSE = "false ";
+    public static final String TRUE_1 = "true 1";
+    public static final String FALSE_1 = "false 1";
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        if (num <= 1) {
-            System.err.println("Illegal Argument");
-            System.exit(-1);
-        }
-        if (num == 2) {
-            System.out.println("true 1");
-            return;
-        }
-        if (num % 2 == 0) {
-            System.out.println("false 1");
-        } else {
-            int count = 1;
-            for (long i = 2; i * i <= num; i++) {
-                if (num % i == 0) {
-                    System.out.println("false " + count);
-                    return;
-                }
-                count++;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int num = scanner.nextInt();
+
+            if (num <= 1) {
+                System.err.println(ILLEGAL_ARGUMENT);
+                System.exit(-1);
             }
-            System.out.println("true " + count);
+
+            if (num == 2) {
+                System.out.println(TRUE_1);
+                return;
+            }
+
+            if (num % 2 == 0) {
+                System.out.println(FALSE_1);
+            } else {
+                int count = 1;
+
+                for (long i = 2; i * i <= num; i++) {
+
+                    if (num % i == 0) {
+                        System.out.println(FALSE + count);
+                        return;
+                    }
+                    count++;
+                }
+
+                System.out.println(TRUE + count);
+            }
         }
     }
 }
