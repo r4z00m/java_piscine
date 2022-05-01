5b325278490415579af47a1101d55fe94496c027
teusebio <teusebio@student.21-school.ru>
2022-05-01 15:26:15 +0300

    remove package

--- day00/ex04/Program.java
+++ day00/ex04/Program.java
@@ -1,41 +1,56 @@
-package ex04;
 
 import java.util.Scanner;
 
 public class Program {
+
+    public static final int SIZE = 10;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] ints = new int[65535];
-        int[] countResult = new int[10];
-        char[] valueResult = new char[10];
-        String s = scanner.nextLine();
+        int[] ints, countResult;
+        char[] valueResult;
+
+        String s;
+        try (Scanner scanner = new Scanner(System.in)) {
+            ints = new int[Character.MAX_VALUE];
+            countResult = new int[SIZE];
+            valueResult = new char[SIZE];
+            s = scanner.nextLine();
+        }
+
         char[] chars = s.toCharArray();
+
         for (int i = 0; i < s.length(); i++) {
             ints[chars[i]]++;
         }
+
         int max, maxIndex;
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < SIZE; i++) {
             max = findMax(ints);
             maxIndex = findMaxIndex(ints, max);
             countResult[i] = ints[maxIndex];
             valueResult[i] = (char) maxIndex;
             ints[maxIndex] = 0;
         }
+
         printResult(countResult, valueResult);
     }
 
     public static void printResult(int[] counts, char[] chars) {
         int d = counts[0];
-        for(int i = 0; i < 10; i++) {
-            if(counts[i] * 10 / d == 10)
+
+        for(int i = 0; i < SIZE; i++) {
+            if(counts[i] * SIZE / d == SIZE)
                 System.out.print(counts[i] + "\t");
         }
+
         System.out.println();
-        for (int i = 10; i > 0; i--) {
-            for (int j = 0; j < 10; j++) {
-                if (counts[j] * 10 / d >= i)
+
+        for (int i = SIZE; i > 0; i--) {
+            for (int j = 0; j < SIZE; j++) {
+                if (counts[j] * SIZE / d >= i)
                     System.out.print("#\t");
-                if (counts[j] * 10 / d == i - 1) {
+                if (counts[j] * SIZE / d == i - 1) {
                     if (counts[j] != 0) {
                         System.out.print(counts[j] + "\t");
                     }
@@ -43,7 +58,8 @@ public class Program {
             }
             System.out.println();
         }
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < SIZE; i++) {
             if (chars[i] != 0) {
                 System.out.print(chars[i] + "\t");
             }
@@ -52,7 +68,7 @@ public class Program {
 
     public static int findMax(int[] ints) {
         int max = 0;
-        for (int i = 0; i < 65535; i++) {
+        for (int i = 0; i < Character.MAX_VALUE; i++) {
             if (ints[i] > max) {
                 max = ints[i];
             }
@@ -61,7 +77,7 @@ public class Program {
     }
 
     public static int findMaxIndex(int[] ints, int max) {
-        for (int i = 0; i < 65535; i++) {
+        for (int i = 0; i < Character.MAX_VALUE; i++) {
             if (ints[i] == max) {
                 return i;
             }
